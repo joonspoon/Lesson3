225c79d2853d5b296844df7fada1227767f96543
joonspoon <joonspoon@joonspoon.com>
2019-03-06 16:10:24 -0800

    Removed pseudocode instructions from High Low.

--- src/exercises/HighLowGame.java
+++ src/exercises/HighLowGame.java
@@ -1,30 +1,6 @@
 package exercises;
 
-import java.util.Random;
-
 public class HighLowGame {
 
-	public static void main(String[] args) {
-		// 3. Change this line to give you a random number between 1 - 100. 
-		int random = new Random().nextInt(5);
-		// 2. Print out the random variable above
-		
-		// 11. do the following 10 times
-		
-			// 1. ask the user for a guess using a pop-up window, and save their response 
-
-			// 4. convert the users’ answer to an int (Integer.parseInt(string))
-			
-			// 5. if the guess is correct
-				// 6. win
-			// 7. if the guess is high
-				// 8. tell them it's too high
-			// 9. if the guess is low
-				// 10. tell them it's too low
-
-		// 12. tell them they lose
-	}
 
 }
-
-
